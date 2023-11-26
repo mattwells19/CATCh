044fbb140a0404c76252af68a6b470fc3eb7486f
Matt Wells <mattwells878@gmail.com>
2023-11-25 22:09:05 -0500

    Try ssr

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,5 @@
 import { defineConfig } from "astro/config";
+import vercel from "@astrojs/vercel/serverless";
 import tailwind from "@astrojs/tailwind";
 
 // import { loadEnv } from "vite";
@@ -6,7 +7,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  // TODO: consider SSR vs static
-  // output: "server",
   integrations: [tailwind()],
+  output: "server",
+  adapter: vercel(),
 });
