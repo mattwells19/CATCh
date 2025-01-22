95a5cb86c3a64931b8ef035d65590431b90683d6
mattwells19 <mattwells878@gmail.com>
2025-01-21 19:32:02 -0500

    don't create two clients

--- src/lib/contentful.ts
+++ src/lib/contentful.ts
@@ -1,18 +1,18 @@
-import contentful from "contentful";
+import contentful, { type CreateClientParams } from "contentful";
 export type { Document as RichTextDocument } from "@contentful/rich-text-types";
 
-const previewContentfulClient = contentful.createClient({
+const previewContentfulClient: CreateClientParams = {
   space: import.meta.env.CONTENTFUL_SPACE_ID,
   accessToken: import.meta.env.CONTENTFUL_PREVIEW_TOKEN,
   host: "preview.contentful.com",
-});
+};
 
-const publicContentfulClient = contentful.createClient({
+const publicContentfulClient: CreateClientParams = {
   space: import.meta.env.CONTENTFUL_SPACE_ID,
   accessToken: import.meta.env.CONTENTFUL_DELIVERY_TOKEN,
   host: "cdn.contentful.com",
-});
+};
 
-export const contentfulClient = import.meta.env.DEV
-  ? previewContentfulClient
-  : publicContentfulClient;
+export const contentfulClient = contentful.createClient(
+  import.meta.env.DEV ? previewContentfulClient : publicContentfulClient,
+);
