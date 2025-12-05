c877fb9a0819a0d1c824cd1a76409944da3491e2
mattwells19 <mattwells878@gmail.com>
2025-12-05 16:51:06 -0500

    volunteer counts

--- src/api/volunteer.ts
+++ src/api/volunteer.ts
@@ -1,4 +1,9 @@
+import { ActionError } from "astro/actions/runtime/shared.js";
+import type { Database } from "~/lib/supabase/database.types";
 import { supabase } from "~/lib/supabase";
+
+export const VOLUNTEER_LIMIT = 3;
+export const TECH_LIMIT = 1;
 
 export const getListingVolunteers = async (listingId: number) => {
   const { data } = await supabase
@@ -26,13 +31,63 @@ export const getListingVolunteers = async (listingId: number) => {
   }));
 };
 
+export const getVolunteerCounts = async (listingIds: Array<number>) => {
+  const { data, error } = await supabase
+    .from("signup")
+    .select("listing_id, role")
+    .in("listing_id", listingIds);
+
+  if (error) {
+    throw error;
+  }
+
+  const signUpSummariesMap = new Map<
+    number,
+    { volunteer: number; tech: number }
+  >(listingIds.map((listingId) => [listingId, { volunteer: 0, tech: 0 }]));
+
+  for (const row of data) {
+    const listingDetails = signUpSummariesMap.get(row.listing_id) ?? {
+      volunteer: 0,
+      tech: 0,
+    };
+
+    signUpSummariesMap.set(row.listing_id, {
+      ...listingDetails,
+      [row.role]: listingDetails[row.role] + 1,
+    });
+  }
+
+  return Object.fromEntries(signUpSummariesMap);
+};
+
 export const volunteer = async (
   listingId: number,
   firstName: string,
   lastName: string,
   email: string,
-  role: string,
+  role: Database["public"]["Enums"]["Volunteer Role"],
 ) => {
+  // validate role is available
+  const { count: roleCount } = await supabase
+    .from("signup")
+    .select("role")
+    .eq("listing_id", listingId)
+    .eq("role", role);
+
+  if (typeof roleCount === "number") {
+    if (
+      (role === "volunteer" && roleCount >= VOLUNTEER_LIMIT) ||
+      (role === "tech" && roleCount >= TECH_LIMIT)
+    ) {
+      throw new ActionError({
+        code: "BAD_REQUEST",
+        message: `The maximum number of people have already signed up for this role: ${role}.`,
+      });
+    }
+  }
+
+  // update/create member signing up
   const { data: existingMemberRecord } = await supabase
     .from("member")
     .select("id")
@@ -55,7 +110,10 @@ export const volunteer = async (
 
   const memberId = data?.at(0)?.id;
   if (!memberId) {
-    throw new Error("Error adding member.");
+    throw new ActionError({
+      code: "FAILED_DEPENDENCY",
+      message: "Error adding member.",
+    });
   }
 
   const { error: signupError } = await supabase.from("signup").upsert([
