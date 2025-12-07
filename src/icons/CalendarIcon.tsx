3faa803d751e52d91cbf8acbcf04095ee446c7a4
mattwells19 <mattwells878@gmail.com>
2025-12-06 21:36:28 -0500

    add live slides

--- src/icons/CalendarIcon.tsx
+++ src/icons/CalendarIcon.tsx
@@ -14,7 +14,7 @@ export const CalendarIcon = (props: React.SVGProps<SVGSVGElement>) => (
       height="15"
       rx="2"
       stroke="currentColor"
-      stroke-width="2"
+      strokeWidth="2"
     />
     <path
       d="M3 10C3 8.11438 3 7.17157 3.58579 6.58579C4.17157 6 5.11438 6 7 6H17C18.8856 6 19.8284 6 20.4142 6.58579C21 7.17157 21 8.11438 21 10H3Z"
@@ -23,14 +23,14 @@ export const CalendarIcon = (props: React.SVGProps<SVGSVGElement>) => (
     <path
       d="M7 3L7 6"
       stroke="currentColor"
-      stroke-width="2"
-      stroke-linecap="round"
+      strokeWidth="2"
+      strokeLinecap="round"
     />
     <path
       d="M17 3L17 6"
       stroke="currentColor"
-      stroke-width="2"
-      stroke-linecap="round"
+      strokeWidth="2"
+      strokeLinecap="round"
     />
     <rect x="7" y="12" width="4" height="2" rx="0.5" fill="currentColor" />
     <rect x="7" y="16" width="4" height="2" rx="0.5" fill="currentColor" />
--- src/pages/internal/slides/_components/Slideshow.tsx
+++ src/pages/internal/slides/_components/Slideshow.tsx
@@ -1,12 +1,141 @@
-import { useCallback, useEffect, useRef, useState } from "react";
+import {
+  isValidElement,
+  useCallback,
+  useEffect,
+  useMemo,
+  useRef,
+  useState,
+  type ReactNode,
+} from "react";
+import { documentToHtmlString } from "@contentful/rich-text-html-renderer";
+
+import type { ClassListing } from "~/api/getClassListings";
 import type { PreshowSlides } from "~/api/getPreshowSlides";
+import type { ShowListing } from "~/api/getShowListings";
+import { CalendarIcon } from "~/icons/CalendarIcon";
+import { formatClassDateRange } from "~/utils/formatClassTime";
+import { formatShowTime } from "~/utils/formatShowTime";
+
+interface UpcomingShowSlidesProps {
+  showListings: Array<ShowListing>;
+}
+
+export const UpcomingShowSlides = ({
+  showListings,
+}: UpcomingShowSlidesProps) => {
+  return (
+    <div className="bg-peach p-5">
+      <p className="text-3xl font-serif font-bold text-primary-purple mb-5">
+        Check out our upcoming shows!
+      </p>
+      <ul className="flex flex-nowrap gap-5">
+        {showListings.map((showListing) => (
+          <li key={showListing.id}>
+            <div className="rounded overflow-hidden shadow-lg p-2 bg-peach flex flex-col gap-4 flex-1 min-w-[315px] max-w-[500px]">
+              <img
+                src={showListing.image}
+                alt={showListing.name}
+                width={724}
+                height={407}
+                className="w-full"
+              />
+
+              <div className="flex flex-col gap-4 px-2 text-lg max-h-[500px]">
+                <div className="flex items-center gap-2 border-b-2 border-light-purple text-primary-purple w-fit mr-auto py-4">
+                  <CalendarIcon name="calendar" className="size-5" />
+
+                  <p className="font-serif font-semibold">
+                    {formatShowTime(showListing.date)}
+                  </p>
+                </div>
+
+                {showListing.showDescription ? (
+                  <div
+                    className="wysiwyg overflow-auto"
+                    dangerouslySetInnerHTML={{
+                      __html: documentToHtmlString(showListing.showDescription),
+                    }}
+                  />
+                ) : null}
+              </div>
+
+              <p className="font-bold px-2">{showListing.price}</p>
+            </div>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+interface UpcomingClassSlidesProps {
+  classListings: Array<ClassListing>;
+}
+
+export const UpcomingClassSlides = ({
+  classListings,
+}: UpcomingClassSlidesProps) => {
+  return (
+    <div className="bg-peach p-5">
+      <p className="text-3xl font-serif font-bold text-primary-purple mb-5">
+        Check out our upcoming classes!
+      </p>
+      <ul className="flex flex-nowrap gap-5">
+        {classListings.map((classListing) => (
+          <li key={classListing.id}>
+            <div className="rounded overflow-hidden shadow-lg p-2 bg-peach flex flex-col gap-4 flex-1 min-w-[315px] max-w-[500px]">
+              <img
+                src={classListing.image}
+                alt={classListing.name}
+                width={724}
+                height={407}
+                className="w-full"
+              />
+
+              <div className="flex flex-col gap-4 px-2 text-lg max-h-[500px]">
+                <div className="flex items-center gap-2 border-b-2 border-light-purple text-primary-purple w-fit mr-auto py-4">
+                  <CalendarIcon name="calendar" className="size-5" />
+
+                  <p className="font-serif font-semibold">
+                    {formatClassDateRange(classListing)}
+                  </p>
+                </div>
+
+                {classListing.classHeader ? (
+                  <p className="font-serif font-bold text-xl mb-2">
+                    {classListing.classHeader}
+                  </p>
+                ) : null}
+
+                {classListing.classDescription ? (
+                  <div
+                    className="wysiwyg overflow-auto"
+                    dangerouslySetInnerHTML={{
+                      __html: documentToHtmlString(
+                        classListing.classDescription,
+                      ),
+                    }}
+                  />
+                ) : null}
+              </div>
+            </div>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
 
 interface SlideProps {
-  slide: PreshowSlides["slides"][number];
+  slide: PreshowSlides["slides"][number] | NonNullable<ReactNode>;
   onVideoEnded: () => void;
 }
 
 const Slide = ({ slide, onVideoEnded }: SlideProps) => {
+  if (typeof slide !== "object" || "contentType" in slide === false) {
+    return slide;
+  }
+
   if (slide.contentType.startsWith("image")) {
     return (
       <img
@@ -36,33 +165,70 @@ const Slide = ({ slide, onVideoEnded }: SlideProps) => {
 interface SlideshowProps {
   slides: PreshowSlides["slides"];
   secondsPerSlide: PreshowSlides["secondsPerSlide"];
+  upcomingShows: Array<ShowListing>;
+  upcomingClasses: Array<ClassListing>;
 }
 
-export const Slideshow = ({ slides, secondsPerSlide }: SlideshowProps) => {
+export const Slideshow = ({
+  slides,
+  secondsPerSlide,
+  upcomingClasses,
+  upcomingShows,
+}: SlideshowProps) => {
   const slideshowRef = useRef<HTMLDivElement | null>(null);
   const [activeSlideIndex, setActiveSlideIndex] = useState<number>(0);
-  const activeSlide = slides.at(activeSlideIndex);
+
+  const allSlides = useMemo(
+    () => [
+      ...slides,
+      <UpcomingClassSlides classListings={upcomingClasses} />,
+      <UpcomingShowSlides showListings={upcomingShows} />,
+    ],
+    [slides, upcomingClasses],
+  );
+  const activeSlide = allSlides.at(activeSlideIndex);
 
   useEffect(() => {
-    if (!activeSlide && slides.length > 0) {
+    if (!activeSlide && allSlides.length > 0) {
       setActiveSlideIndex(0);
     }
-  }, [activeSlide, slides]);
+  }, [activeSlide, allSlides]);
 
   const handleNextSlide = useCallback(() => {
     setActiveSlideIndex((prevIdx) =>
-      prevIdx === slides.length - 1 ? 0 : prevIdx + 1,
+      prevIdx === allSlides.length - 1 ? 0 : prevIdx + 1,
     );
-  }, [slides]);
+  }, [allSlides]);
 
   useEffect(() => {
-    if (activeSlide && activeSlide.contentType.startsWith("image")) {
-      const t = setTimeout(handleNextSlide, secondsPerSlide * 1000);
+    if (!activeSlide) {
+      return;
+    }
+
+    // when showing a video
+    if (
+      typeof activeSlide === "object" &&
+      "contentType" in activeSlide &&
+      activeSlide.contentType.startsWith("video")
+    ) {
+      return;
+    }
+
+    // when showing live show/class events
+    if (isValidElement(activeSlide)) {
+      const t = setTimeout(handleNextSlide, secondsPerSlide * 2 * 1000);
 
       return () => {
         clearTimeout(t);
       };
     }
+
+    // when showing an image
+    const t = setTimeout(handleNextSlide, secondsPerSlide * 1000);
+
+    return () => {
+      clearTimeout(t);
+    };
   }, [activeSlide, handleNextSlide, secondsPerSlide]);
 
   if (!activeSlide) {
