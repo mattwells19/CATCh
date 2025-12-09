5897e7e4e4478b4aa33122a85694439aabaef295
mattwells19 <mattwells878@gmail.com>
2025-12-09 13:30:31 -0500

    case insensitive email checks, updated verbiage, and more form validation

--- src/actions/volunteer.ts
+++ src/actions/volunteer.ts
@@ -9,9 +9,28 @@ export const volunteer = {
       listingId: z.number(),
       listingName: z.string(),
       listingDate: z.string().datetime(),
-      firstName: z.string(),
-      lastName: z.string(),
-      email: z.string().email(),
+      firstName: z
+        .string()
+        .trim()
+        .min(1, "Your first name has to have at least one letter.")
+        .max(
+          35,
+          "Sorry, but your first name is too long. Max is 35 characters.",
+        ),
+      lastName: z
+        .string()
+        .trim()
+        .min(1, "Your last name has to have at least one letter.")
+        .max(
+          35,
+          "Sorry, but your last name is too long. Max is 35 characters.",
+        ),
+      email: z
+        .string()
+        .trim()
+        .email()
+        .toLowerCase()
+        .max(254, "Email addresses cannot be longer than 254 characters."),
       role: z.enum(["tech", "volunteer"]),
     }),
     handler: (input) =>
@@ -30,7 +49,12 @@ export const volunteer = {
     input: z.object({
       listingId: z.number(),
       memberId: z.number(),
-      email: z.string().email(),
+      email: z
+        .string()
+        .trim()
+        .email()
+        .toLowerCase()
+        .max(254, "Email addresses cannot be longer than 254 characters."),
     }),
     handler: (input) =>
       removeSignUp(input.listingId, input.memberId, input.email),
