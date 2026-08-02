3f3fdb2d614038ef5cd601c70dc8eef3f28e83ba
mattwells19 <mattwells878@gmail.com>
2026-08-02 12:08:03 -0400

    replace redirect with landing page

--- astro.config.mjs
+++ astro.config.mjs
@@ -40,10 +40,6 @@ export default defineConfig({
     "/classes/improv-performance-track-2": "/classes/performance-track",
     "/classes/improv-performance-track-3": "/classes/performance-track",
     "/improv-classes/improv-performance-track-1": "/classes/performance-track",
-    "/internal/volunteer": {
-      status: 301,
-      destination: "https://www.venvyapp.com/public/volunteer",
-    },
   },
   image: {
     domains: ["tlt-events.s3.amazonaws.com", "catch.theater"],
